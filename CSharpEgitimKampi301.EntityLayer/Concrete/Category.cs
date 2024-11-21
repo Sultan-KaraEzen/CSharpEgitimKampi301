aa6a65dbebe7e434a2fadd47181aaf0c0cd3393e
Sultan-KaraEzen <sultankaraezen@hotmail.com>
2024-11-21 13:44:07 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+
+    }
+    /*Field-Variable-Property*/
+    /*
+     int x sınıfın içinde tanımlanırsa ->field
+    sonuna get ve set değeri alırsa -->property
+    method içinde tanımlanırsa -->variable
+    prop tab property oluşturur.
+     */
+    
+}
