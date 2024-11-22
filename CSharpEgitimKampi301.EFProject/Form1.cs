1d4c1adaf9bfc65c781d8a90b8549153b0f471ed
Sultan-KaraEzen <sultankaraezen@hotmail.com>
2024-11-22 15:53:54 +0300

    Guide formu düzenlendi

--- CSharpEgitimKampi301.EFProject/Form1.cs
+++ CSharpEgitimKampi301.EFProject/Form1.cs
@@ -16,5 +16,53 @@ namespace CSharpEgitimKampi301.EFProject
         {
             InitializeComponent();
         }
+
+        EgitimKampiEFTravelDbEntities db = new EgitimKampiEFTravelDbEntities();
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+           
+            var values=db.Guide.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Guide guide = new Guide();
+            guide.GuideName=textName.Text;
+            guide.GuideSurname=textSurname.Text;
+            db.Guide.Add(guide);
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla eklendi");
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(textId.Text);
+            var removeValue=db.Guide.Find(id);
+            db.Guide.Remove(removeValue);
+            MessageBox.Show("Rehber başarıyla silindi");
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(textId.Text);
+            var updateValue = db.Guide.Find(id);
+            updateValue.GuideName = textName.Text;
+            updateValue.GuideSurname = textSurname.Text;
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla güncellendi","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+
+        private void btnGetByID_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(textId.Text);
+            var values=db.Guide.Where(x=>x.GuideId==id).ToList();
+            dataGridView1.DataSource = values;
+
+        }
     }
 }
